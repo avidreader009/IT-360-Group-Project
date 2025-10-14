498467f838692f1962f6ead35dc490bdd555f4cd
avidreader009 <63505327+avidreader009@users.noreply.github.com>
2025-10-13 20:06:23 -0500

    Added ps1 script via upload
    contains all of the get commands. exporting to the csv requires further testing and needs to be added for the last two lines

---
+++ script.ps1
@@ -0,0 +1,11 @@
+﻿#get hardware information
+Get-ComputerInfo -Property "Windows*", "Bios*", "Os*", "TimeZone" | Export-csv -Path ".\Information.csv" -NoTypeInformation
+
+#get running processes and version
+Get-Process -FileVersionInfo | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
+
+#get io devices
+Get-PnpDevice | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
+
+#get system logs
+Get-EventLog | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
