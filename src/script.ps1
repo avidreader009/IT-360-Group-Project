da3aba41ed0651a1589b52cfa0ab3e4df3f29170
avidreader009 <63505327+avidreader009@users.noreply.github.com>
2025-11-17 19:32:20 -0600

    Update script.ps1
    Changed output file to txt from csv. added try catch block for get-process command. added command to inform user when script finishes running

--- src/script.ps1
+++ src/script.ps1
@@ -1,11 +1,35 @@
-﻿#get hardware information
-Get-ComputerInfo -Property "Windows*", "Bios*", "Os*", "TimeZone" | Export-csv -Path ".\Information.csv" -NoTypeInformation
+# output hardware information header to file
+"Hardware information: " | Out-File -FilePath .\Information.txt
 
-#get running processes and version
-Get-Process -FileVersionInfo | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
+# get hardware information
+Get-ComputerInfo -Property "Windows*", "Bios*", "Os*", "TimeZone" | Out-File -FilePath .\Information.txt -Append
 
-#get io devices
-Get-PnpDevice | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
+# output running processes header to file
+"Running Processes: " | Out-File -FilePath .\Information.txt -Append
 
-#get system logs
-Get-EventLog | Export-Csv -Path ".\Information.csv" -NoTypeInformation -Append
+# get running processes and version
+Get-Process | ForEach-Object {
+    try {
+        ($_.FileVersionInfo) | Out-File -FilePath .\Information.txt -Append
+    }
+    catch {
+        # error is that process has no version information
+        # output the processes without the version information
+        $_.ProcessName | Out-File -FilePath .\Information.txt -Append
+    }
+}
+
+# output io devices header to file
+"I/O Devices: " | Out-File -FilePath .\Information.txt -Append
+
+# get io devices
+Get-PnpDevice | Out-File -FilePath .\Information.txt -Append
+
+# output system logs header to file
+"System Logs: " | Out-File -FilePath .\Information.txt -Append
+
+# get system log
+Get-EventLog -LogName System| Out-File -FilePath .\Information.txt -Append
+
+#write output to host to inform user that commands have finished running
+Write-Output "Get commands have finished running. Check Information.txt"
